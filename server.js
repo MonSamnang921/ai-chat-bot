107a38789ada5f3c5440210cd823cd8afe1fe9f1
MonSamnang921 <ooo060565719@gmail.com>
2026-08-13 02:24:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// កែកូដត្រង់ចំណុច Serve Static Files
+app.use(express.json());
+
+// Serve static files ចេញពី folder បច្ចុប្បន្ន (Root) ឬ public
 app.use(express.static(__dirname));
 
+// Route សម្រាប់ទំព័រដើម
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// កំណត់ Port សម្រាប់ Render
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
